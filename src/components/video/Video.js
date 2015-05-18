f739f3a193b7362400d08b485dfcf25afb56df3d
Matt Conrad <mmconrad@gmail.com>
2015-05-18 14:22:20 -0700

    Updates video component on main and creates new one for separate video view

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -21,3 +21,15 @@ var Video = React.createClass({
 });
 
 module.exports = Video;
+
+
+
+return (
+  <li>
+    <iframe title="YouTube video player"  type="text/html" 
+      width="640" height="390" 
+      src={url} frameBorder="0" allowFullScreen>
+    </iframe>
+    <p>{this.props.data.title} <a href={this.props.data.url} /> </p>
+    <p>by {this.props.data.author}</p>
+    <p>View: {this.props.data.views} - {this.props.data.date}</p>
